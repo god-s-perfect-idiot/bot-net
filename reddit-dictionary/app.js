c7cda9674323b8b9edbafb68f6b257ec07c94e82
god-s-perfect-idiot <samarmusthafa2014@gmail.com>
2021-11-28 15:50:29 +0530

    - worked on TL Bot - worked on reddit crawler

--- reddit-dictionary/app.js
+++ reddit-dictionary/app.js
@@ -1,22 +1,37 @@
-const https = require('https')
-const options = {
-  port: 443,
-  method: 'GET'
+const puppeteer = require('puppeteer-extra');
+
+// add stealth plugin and use defaults (all evasion techniques)
+const StealthPlugin = require('puppeteer-extra-plugin-stealth');
+puppeteer.use(StealthPlugin());
+
+// puppeteer usage as normal
+(async () => {
+  const browser = await puppeteer.launch({
+    headless: false  
+  });
+  const page = await browser.newPage();
+  await page.goto('https://reddit.com');
+  console.log(await getHrefs(page));
+  await wait(399999);
+  await browser.close();
+})();
+
+async function getHrefs(page) {
+  const urls = await page.$$eval('a', anchors => [].map.call(anchors, a => a.href));
+  return urls.filter(url => {
+    try {
+      return new URL(url).hostname === 'www.reddit.com'
+    }
+    catch(e) {
+      return false;
+    }      
+  })
 }
 
-
-options.hostname = 'reddit.com'
-
-const req = https.request(options, res => {
-  console.log(res)
-
-  res.on('data', d => {
-    console.log(d)
+const wait = async (ms) => {
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      res()
+    }, ms)
   })
-})
-
-req.on('error', error => {
-  console.error(error)
-})
-
-req.end()
+}
